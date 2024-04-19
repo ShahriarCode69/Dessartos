574db629912c378e3b3e7764e484761169be318f
ShahriarCode <shahriarcode69@gmail.com>
2024-04-19 23:47:17 +0600

    reviews sections are done just need to write some content

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import '../styles/components/header.css'
 import '../styles/components/hero.css'
 import '../styles/components/about.css'
 import '../styles/components/benefit.css'
+import '../styles/components/reviews.css'
 import '../styles/components/mobile-nav.css'
 import '../styles/utils.css'
 
